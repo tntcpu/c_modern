08dc95e39628e6171d94fdddfd381e2f002ae091
tntcpu <gpscpu@gmail.com>
2024-07-01 19:58:25 +0800

    test

--- 4_chart/main.c
+++ 4_chart/main.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
-//int main() {
-//    int i = 10;
-//    int j = 20;
-//    printf("%d %d\n",&i,&j);
-//    printf("Hello, World!\n");
-//    return 0;
-//}
+int main() {
+   int i = 10;
+   int j = 20;
+   printf("%d %d\n",&i,&j);
+   printf("Hello, World!\n");
+   return 0;
+}
