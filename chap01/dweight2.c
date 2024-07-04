ca6334935b00473e5a25894b9e1bfcacce9c802d
tntcpu <gpscpu@gmail.com>
2024-07-04 19:57:54 +0800

    commit

--- chap01/dweight2.c
+++ chap01/dweight2.c
@@ -1,5 +1,6 @@
 // 根据用户输入计算箱子重量
 #include <stdio.h>
+#define INCHES_PER_POUND 166
 int main(void)
 {
     int height, length, weight, width, volume;
@@ -10,8 +11,8 @@ int main(void)
     printf("width:");
     scanf("%d",&width);
     volume = height * length * width;
-    weight = (volume + 165) / 166;
-    printf("Dimensions: %dx%dx%d\n", length, weight, height);
+    weight = (volume + 165) / INCHES_PER_POUND;
+    printf("Dimensions: %dx%dx%d\n", length, width, height);
     printf("Volume (cubic inches): %d\n", volume);
     printf("Dimensional weight (pounds): %d\n", weight);
     return 0;
